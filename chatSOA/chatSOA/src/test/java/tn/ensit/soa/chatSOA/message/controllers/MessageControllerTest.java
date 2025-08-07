1dafa0663b9615e7d9901de446b7365386971a55
ines Tmimi <ines.tmimi@ensit.u-tunis.tn>
2025-08-07 16:40:59 +0100

    add gradle

--- chatSOA/chatSOA/src/test/java/tn/ensit/soa/chatSOA/message/controllers/MessageControllerTest.java
+++ chatSOA/chatSOA/src/test/java/tn/ensit/soa/chatSOA/message/controllers/MessageControllerTest.java
@@ -1,34 +1,44 @@
 package tn.ensit.soa.chatSOA.message.controllers;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import tn.ensit.soa.chatSOA.message.dto.MessageDto;
-
+import tn.ensit.soa.chatSOA.message.entities.Message;
+import tn.ensit.soa.chatSOA.message.services.MessageService;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MessageController.class)
 class MessageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private MessageService messageService;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public MessageService messageService() {
+            return Mockito.mock(MessageService.class);
+        }
+    }
 
     @Test
-    void testCreateMessage_shouldReturn200() throws Exception {
-        MessageDto dto = new MessageDto("general", 1L, 2L, "Hello without mockito!");
+    void testCreateMessage() throws Exception {
+        Message message = new Message("channel1", 1L, 2L, "Hello, World!");
+        Mockito.when(messageService.saveMessage(Mockito.any(MessageDto.class))).thenReturn(message);
 
         mockMvc.perform(post("/messages/send")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(dto)))
+                        .content("{\"channelId\":\"channel1\",\"senderId\":1,\"receiverId\":2,\"content\":\"Hello, World!\"}"))
                 .andExpect(status().isOk());
     }
 }
+
