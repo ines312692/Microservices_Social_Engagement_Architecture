47119560df170672735751213adc7efd74d91fa5
ines Tmimi <ines.tmimi@ensit.u-tunis.tn>
2025-08-07 16:14:47 +0100

    add gradle

---
+++ chatSOA/chatSOA/src/test/java/tn/ensit/soa/chatSOA/message/controllers/MessageControllerTest.java
@@ -0,0 +1,34 @@
+package tn.ensit.soa.chatSOA.message.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tn.ensit.soa.chatSOA.message.dto.MessageDto;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MessageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testCreateMessage_shouldReturn200() throws Exception {
+        MessageDto dto = new MessageDto("general", 1L, 2L, "Hello without mockito!");
+
+        mockMvc.perform(post("/messages/send")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(status().isOk());
+    }
+}
